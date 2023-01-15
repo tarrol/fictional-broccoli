a5b45f195f8ae8daf25a2e79c6ed90540fbe569e
Tarrol <quickandrey@gmail.com>
2023-01-15 15:13:38 -0800

    Created models for SQL tables, installed packages

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,21 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
     // define columns
+    id: { //defining category table column named 'id' so that we can create expected behaviors like whether or not we're expecting an integer, whether it can be blank, etc
+      type: DataTypes.INTEGER,
+      allowNULL: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: { //above, but for category_name
+      type: DataTypes.STRING,
+      allowNULL: false,
+    }
   },
   {
     sequelize,
--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,43 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id{
+  type: DataTypes.INTEGER,
+  allowNULL: false,
+  primaryKey: true,
+  autoIncrement: true,
+},
+  product_name{
+  type: DataTypes.STRING,
+  allowNULL: false,
+},
+  price{
+  type: DataTypes.DECIMAL(10, 2),
+  allowNULL: false,
+  validate: {
+    isDecimal: true,
+  },
+},
+  stock{
+  type: DataTypes.INTEGER,
+  allowNULL: false,
+  validate: {
+    isNumeric: true,
+  },
+},
+  category_id{
+  type: DataTypes.INTEGER,
+  allowNULL: true,
+  references: {
+    model: 'category',
+    key: 'id',
+  },
+},
   },
   {
     sequelize,
