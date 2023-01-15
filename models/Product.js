8dcfdb580fd7f18feba73a81069ac78df777e219
Tarrol <quickandrey@gmail.com>
2023-01-15 15:22:30 -0800

    Last missing colons

--- models/Product.js
+++ models/Product.js
@@ -10,37 +10,37 @@ class Product extends Model { }
 Product.init(
   {
     id: {
-  type: DataTypes.INTEGER,
-  allowNULL: false,
-  primaryKey: true,
-  autoIncrement: true,
-},
-  product_name{
-  type: DataTypes.STRING,
-  allowNULL: false,
-},
-  price: {
-  type: DataTypes.DECIMAL(10, 2),
-  allowNULL: false,
-  validate: {
-    isDecimal: true,
-  },
-},
-  stock: {
-  type: DataTypes.INTEGER,
-  allowNULL: false,
-  validate: {
-    isNumeric: true,
-  },
-},
-  category_id: {
-  type: DataTypes.INTEGER,
-  allowNULL: true,
-  references: {
-    model: 'category',
-    key: 'id',
-  },
-},
+      type: DataTypes.INTEGER,
+      allowNULL: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNULL: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNULL: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNULL: false,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNULL: true,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
