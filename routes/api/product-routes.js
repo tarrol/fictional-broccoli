863a91e417e213627c8874178e331771c80f38e5
Tarrol <quickandrey@gmail.com>
2023-01-15 16:05:16 -0800

    Added successful get requests for products and tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,24 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category, { model: Tag, through: ProductTag, }],
+  })
+    // be sure to include its associated Category and Tag data
+    .then((product) => res.json(product))
+    .catch((err) => res.status(500).json(err))
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: { id: req.params.id, },
+    include: [Category, { model: Tag, through: ProductTag, }],
+  })
+    // be sure to include its associated Category and Tag data
+    .then((product) => res.json(product))
+    .catch((err) => res.status(400).json(err))
 });
 
 // create new product
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,12 +5,23 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{ model: Product, through: ProductTag, }]
+  })
+    // be sure to include its associated Product data
+    .then((tags) => res.status(200).json(tags))
+    .catch((err) => res.status(500).json(err))
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: { id: req.params.id, },
+    include: [{ model: Product, through: ProductTag, },],
+  })
+    // be sure to include its associated Product data
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err))
 });
 
 router.post('/', (req, res) => {
